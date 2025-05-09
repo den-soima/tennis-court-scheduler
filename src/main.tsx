882493e29175551a366634f67ab9bb1b5a99f11d
dso <dso.sdeve@gmail.com>
2025-05-09 13:31:52 +0300

    add BASE_URL

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 // import './App.module.scss';
 import App from './App';
+import {createRoot} from 'react-dom/client';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,6 +7,7 @@ declare module '*.scss' {
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+    readonly BASE_URL: string;
     VITE_API_URL?: string;
     // інші VITE_* змінні
 }
